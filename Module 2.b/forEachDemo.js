88a8c30871534dda9ac67323fffb195b22baa0d7
shrishtigarg566 <98161093+shrishtigarg566@users.noreply.github.com>
2022-01-28 15:27:51 +0530

    Add files via upload

---
+++ Module 2.b/forEachDemo.js
@@ -0,0 +1,10 @@
+import { LightningElement } from 'lwc';
+
+export default class IterationDemo extends LightningElement {
+    contacts=[
+        {Index:'0',Id:'C1',Name:'John Abraham',Email:'ja@gmail.com'},
+        {Index:'1',Id:'C2',Name:'Brad Pitt',Email:'bpitt@gmail.com'},
+        {Index:'2',Id:'C3',Name:'Angelina Jolie',Email:'angelj@gmail.com'},
+        {Index:'3',Id:'C4',Name:'Peter Parker',Email:'spiderman@marvelcomics.com'},
+    ];
+}
